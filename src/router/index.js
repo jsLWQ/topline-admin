a82f247a72ae08679c539afee7e934344ea86067
jsLWQ <js8491709937@163.com>
2019-06-24 10:47:21 +0800

    使用路由拦截器处理页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const router = new Router({
       component: () => import('@/views/layout'),
       children: [
         {
+          name: 'home',
           path: '',
           component: () => import('@/views/home')
         }
@@ -22,5 +23,23 @@ const router = new Router({
       component: () => import('@/views/login')
     }
   ]
+})
+router.beforeEach((to, from, next) => {
+  const userInfo = window.localStorage.getItem('user-info')
+  //  判断如果不是登录页，则检查本地存储中是否有user-info，然后取反，没有值则进入，去跳转到登录页，有则next()
+  //  如果是登录页，也检查本地存储是否有user-info，有则不让其进入登录页，没有才让进入
+  if (to.name !== 'login') {
+    if (!userInfo) {
+      next({ path: '/login' })
+    } else {
+      next()
+    }
+  } else {
+    if (userInfo) {
+      next(false)
+    } else {
+      next()
+    }
+  }
 })
 export default router
