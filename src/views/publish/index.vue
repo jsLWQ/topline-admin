6a67045e33715e79034fd8bec2c8bfe771124767
jsLWQ <js8491709937@163.com>
2019-06-27 21:55:04 +0800

    发布文章-走通发布流程

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -4,9 +4,14 @@
       <div slot="header" class="clearfix">
         <span>发表文章</span>
       </div>
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="form"
+      v-loading="false"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(0, 0, 0, 0.8)"
+      :model="form" label-width="80px">
         <el-form-item label="标题">
-          <el-input v-model="form.name" placeholder="文章名称"></el-input>
+          <el-input v-model="form.title" placeholder="文章名称"></el-input>
         </el-form-item>
         <el-form-item label="内容">
           <el-input type="textarea" v-model="form.content"></el-input>
@@ -14,34 +19,65 @@
         <el-form-item label="封面">
         </el-form-item>
         <el-form-item label="文章频道">
-          <el-select v-model="form.region" placeholder="请选择文章频道">
+          <el-select v-model="form.channel_id" placeholder="请选择文章频道">
             <el-option label="区域一" value="shanghai"></el-option>
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
         </el-form-item>
-        <hr>
-        <el-form-item class="operation">
-          <el-button type="primary">发布</el-button>
-          <el-button type="info">草稿</el-button>
-        </el-form-item>
       </el-form>
+        <hr>
+      <div class="operation">
+        <el-button type="primary" @click="releaseEssay(false)">发布</el-button>
+        <el-button type="info">草稿</el-button>
+      </div>
     </el-card>
   </div>
 </template>
 <script>
 export default {
+  name: 'Apppublish',
   data () {
     return {
       form: {
-        name: '',
-        content: ''
+        title: '', // 文章状态
+        content: '', //  文章内容
+        channel_id: 3, // 文章频道
+        cover: { //  封面
+          type: 0,
+          images: []
+        }
       }
+    }
+  },
+  methods: {
+    // 发布文章
+    releaseEssay (draft) {
+      this.$axios({
+        method: 'POST',
+        url: `/articles`,
+        data: this.form,
+        params: {
+          draft
+        }
+      }).then(res => {
+        console.log(res)
+        this.$message({
+          message: '恭喜你，发布成功',
+          type: 'success'
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message({
+          message: '恭喜你，发布失败',
+          type: 'success'
+        })
+      })
     }
   }
 }
 </script>
 <style lang="less" scoped>
 .operation {
-  margin-top: 20px;
+  margin-top: 39px;
 }
 </style>
